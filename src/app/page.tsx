8828407fe7f8273a37f010fc8244f3c3683d28da
amscu-dev <amarinescu.dev@gmail.com>
2026-04-02 14:46:06 +0300

    Refactor JSX structure in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ export const revalidate = 0;
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black"><main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <Suspense fallback={<Loader />}>
           <FeatureTest />
         </Suspense>
@@ -22,8 +23,7 @@ export default function Home() {
           priority
         />
 
-        <div className="flex flex-col items-center 
-          gap-6 text-center sm:items-start sm:text-left">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
