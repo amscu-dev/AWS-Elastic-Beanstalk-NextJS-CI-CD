899846ec1ad349c3d6886f1ec74873eecd261788
amscu-dev <amarinescu.dev@gmail.com>
2026-04-03 19:28:23 +0300

    chore(components): added a test card component

---
+++ __tests__/AppVersion.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import AppVersion from "@/components/AppVersion";
+
+jest.mock("../package.json", () => ({
+  version: "1.0.0",
+}));
+
+describe("AppVersion", () => {
+  test("renders correct version from package.json", () => {
+    render(<AppVersion />);
+    expect(screen.getByText("app-version@1.0.0")).toBeInTheDocument();
+  });
+
+  test("renders link with correct href", () => {
+    render(<AppVersion />);
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute(
+      "href",
+      "https://github.com/amscu-dev/AWS-Elastic-Beanstalk-NextJS-CI-CD",
+    );
+  });
+});
